320ca2db5f5ff608f4f665232c7071f6e61eea48
Shaileja-V <shaileja2002@gmail.com>
2024-11-22 17:00:27 +0300

    Fixed chatbot not appearing

--- public/js/SignUp.js
+++ public/js/SignUp.js
@@ -6,7 +6,6 @@ document.getElementById('signup-form').addEventListener('submit', async e=>{
     const confirmPassword = document.getElementById('confirm-password').value;
 
     console.log(name, email, password)
-
     if(!name || !email || !password || !confirmPassword){
         showToast('Please fill in all the fields','warning')
         return;
@@ -30,16 +29,12 @@ document.getElementById('signup-form').addEventListener('submit', async e=>{
         body: JSON.stringify({name,email,password})
     })
 
-
     const data = await response.json();;
     showToast(data.message,'info')
     if(data.success){
         window.location.href = '/login'
     }  
 })
-
-
-
 function verifyPassword(password) {
     const strongRegex = new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})");
     return strongRegex.test(password) ? true : false;    //returning boolean value of the password validation check
